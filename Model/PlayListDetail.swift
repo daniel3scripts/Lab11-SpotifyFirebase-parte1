7cd135d90a17cac5a21a6ab9222c12056e882dba
MAC15 <mac15@MacBook-Air-de-MAC15.local>
2022-06-04 02:58:36 -0500

    spotifire-1

---
+++ Model/PlayListDetail.swift
@@ -0,0 +1,42 @@
+
+
+import Foundation
+
+struct PlaylistDetail: Codable {
+    let name: String
+    let description: String
+    let followers: Followers
+    let tracks: TracksDetail
+}
+
+struct Followers: Codable {
+    let total: Int
+}
+
+struct TracksDetail: Codable {
+    let items: [ItemDetail]
+}
+
+struct ItemDetail: Codable {
+    let track: Track
+}
+
+struct Track: Codable {
+    let album: Album
+}
+
+struct Album: Codable {
+    let name: String
+    let artists: [Artists]
+    let images: [ImageDetail]
+}
+
+struct Artists: Codable {
+    let name: String
+}
+
+struct ImageDetail: Codable {
+    let height: Int?
+    let url: String?
+    let width: Int?
+}
---
+++ ViewCell/ItemPlaylistDetailTableViewCell.swift
@@ -0,0 +1,18 @@
+
+import UIKit
+
+class ItemPlaylistDetailTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblAuthor: UILabel!
+    @IBOutlet weak var coverImage: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
